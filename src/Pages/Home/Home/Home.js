a54da6c4ec4145cf8e3138fc8e737c569fcb9da7
UmmeNafisa <pappanafisa@gmail.comm>
2021-10-18 02:00:59 +0600

    banner added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,47 +1,68 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Col, Container, Row } from 'react-bootstrap';
 import banner1 from '../../../Images/banners/Slider1.jpg'
 import banner2 from '../../../Images/banners/Slider2.jpg'
 import banner3 from '../../../Images/banners/Slider3.jpg'
+import './Home.css'
 
 const Home = () => {
     return (
         <Carousel fade>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100 banner-img"
                     src={banner1}
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h1>You can't always be there.</h1>
-                    <h1>But we can.</h1>
-                    <p> CareGivers offers an individualized approach to keep seniors safe and sound at home, instead of anywhere else</p>
-                    <button className="btn btn-info text-white">OUR CARE SERVICES </button>
+                    <Container fluid>
+                        <Row>
+                            <Col className="text-start" lg={8}>
+                                <h1>You can't always be there.</h1>
+                                <h1>But we can.</h1>
+                                <p className="w-50"> CareGivers offers an individualized approach to keep seniors safe and sound at home, instead of anywhere else</p>
+                                <button className="btn btn-info text-white btn-hover">OUR CARE SERVICES </button>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100 banner-img"
                     src={banner2}
                     alt="Second slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <Container fluid>
+                        <Row>
+                            <Col className="text-start" lg={8}>
+                                <h1>We give seniors the love they deserve. </h1>
+                                <p className="w-50"> CareGivers offers an individualized approach to keep seniors safe and sound at home, instead of anywhere else</p>
+                                <button className="btn btn-info text-white btn-hover">OUR CARE SERVICES </button>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100 banner-img"
                     src={banner3}
                     alt="Third slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <Container fluid>
+                        <Row>
+                            <Col className="text-start" lg={8}>
+                                <h1>Let's enrich the lives of seniors with kindness and joy!</h1>
+                                <p className="w-50"> CareGivers offers an individualized approach to keep seniors safe and sound at home, instead of anywhere else</p>
+                                <button className="btn btn-info text-white btn-hover">OUR CARE SERVICES </button>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
